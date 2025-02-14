e980283f2f92da6646866b62caea112f6165fba4
shawonNirob <shawonnirob16@gmail.com>
2025-02-14 15:38:07 +0600

    add SQLAlchemy Base for ORM models

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,15 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+
+Base = declarative_base()
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print(f"DATABASE_URL: {DATABASE_URL}")
-
-if DATABASE_URL is None:
+if not DATABASE_URL:
     raise ValueError("ERROR: DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
